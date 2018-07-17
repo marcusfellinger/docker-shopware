e849415a812b8fb97fb2a2bf3209054bac16f8f4
marcusfellinger <m.fellinger@signundsinn.de>
2018-07-17 17:58:16 +0200

    Add version 5.4.5 php7

--- download-shopware.sh
+++ download-shopware.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -O /shopware/shop.zip http://releases.s3.shopware.com.s3.amazonaws.com/install_5.4.3_2cc37d9929992efe320de811902bb445bab0de5a.zip
+wget -O /shopware/shop.zip http://releases.s3.shopware.com.s3.amazonaws.com/install_5.4.5_6847c0845f0f97230aa05c7294fa726a96dda3ff.zip
